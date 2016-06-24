5386c471805f32a7b3d49567cdd36ce85e1391d9
Godje <comepot.4anell@gmail.com>
2016-06-23 20:51:10 -0700

    1.0
    01020312

--- main.js
+++ main.js
@@ -5,83 +5,103 @@ var canvasBody = document.getElementById("canvas"),
     h = canvasBody.height = window.innerHeight,
 
     opts = {
-      amount: 12,
-      opacity: 0.3, //Opacity of the hexagons
-      color: "rgba(0,0,0,alpha)", //Black color
-      speed: 4000, //Higher = slower
-      size: 40
+      amount: 80,
+
+      len: 200,
+      size: 10,
+
+      baseTime: 100,
+      addedTime: 10,
+
+      color: "rgba(255,255,255,alpha)",
+      opacity: 1,
+      bgColor: "#8F1D21",
+
+      cx: w/2,
+      cy: h/2,
     },
 
-    baseRad = (Math.PI * 2 / 6) * 5,
-    tick = 0;
-
-    dieX = w + opts.size * 2;
-    dieY = w + opts.size * 2;
-
-    hexagons = [];
+    tick = 0,
+    steam = [],
+    baseRad = Math.PI * 2 / 6;
 
 function loop(){
   window.requestAnimationFrame(loop);
 
   ++tick;
-  console.log(tick);
-  canvas.fillStyle = "white";
+
+  canvas.fillStyle = opts.bgColor;
   canvas.fillRect(0,0,w,h);
+  canvas.fillStyle = opts.color.replace("alpha", opts.opacity)
 
+  if(opts.amount!==0){
+    steam.push( new Arom );
+    steam.map(function (arom){ arom.reset(); });
+    opts.amount--;
+    console.log(steam);
+  }
 
-  hexagons.push(new Hexagon);
-  hexagons.map( function( hexagon ){hexagon.step();});
+  steam.map( function( arom ){ arom.step();});
 }
 
-function Hexagon(){
+function Arom(){
 
   this.reset();
 }
 
-Hexagon.prototype.reset = function () {
-  this.startX = -100;
-  this.startY = -200;
+Arom.prototype.reset = function(){
+
+  this.currentX = 0;
+  this.currentY = 0;
 
   this.x = 0;
   this.y = 0;
 
-  this.time = 0;
-
   this.addedX = 0;
   this.addedY = 0;
-  this.rad = 0;
 
-  this.color = opts.color.replace("alpha", opts.opacity);
-  this.targetTime = 600;
+  this.rad = 0;
+  this.cumulativeTime = 0;
+
+  this.color = opts.color.replace("aplha", opts.opacity);
 
   this.beginPhase();
 };
 
-Hexagon.prototype.beginPhase = function () {
+Arom.prototype.beginPhase = function(){
   this.x += this.addedX;
   this.y += this.addedY;
 
-  this.rad += baseRad + (Math.random() * (Math.PI * 2 / 18));
+  this.time = 0;
+  this.targetTime = ( opts.baseTime + opts.addedTime * Math.random()) |0;
+
+  this.rad += baseRad + (Math.random() * 10);
   this.addedX = Math.cos( this.rad );
   this.addedY = Math.sin( this.rad );
 
-  this.time = 0;
-};
+  console.log(this.currentX > w)
+}
 
-Hexagon.prototype.step = function () {
+Arom.prototype.step = function(){
+
   ++this.time;
+  ++this.cumulativeTime;
 
-  if( this.time >= this.targetTime || this.x > dieX || this.y > dieY){
-    this.reset();
+  if( this.time >= this.targetTime ){
+    this.beginPhase();
   };
 
   var prop = this.time / this.targetTime,
       x = this.addedX * prop,
       y = this.addedY * prop;
 
-  canvas.fillStyle = this.color;
-  canvas.fillRect( this.startX + (this.x + x), this.startY + (this.y + y), opts.size, opts.size);
-  console.log(this.startX + (this.x + x) * opts.speed);
-};
+  canvas.fillStyle = opts.color.replace("aplha", opts.opacity);
+  canvas.beginPath();
+  canvas.arc( opts.cx + (this.x + x) * opts.len, opts.cy + (this.y + y) * opts.len, opts.size, 0, Math.PI * 2 / (Math.random()));
+  canvas.fill();
+  console.log()
+  this.currentX = opts.cx + (this.x + x) * opts.len;
+  this.currentY = opts.cy + (this.y + y) * opts.len;
+}
 
 loop();
